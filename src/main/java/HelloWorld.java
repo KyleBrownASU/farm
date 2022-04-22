484cf634e73def9d63f446b7e3376aa8d6e714ac
KyleBrownASU <97472449+KyleBrownASU@users.noreply.github.com>
2022-04-21 19:29:07 -0700

    finished main

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,12 @@
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class HelloWorld {
 
 
     public static void main(String[] args) {
-        LinkedList<Farm> farmList = new LinkedList();
+        LinkedList<Farm> farmList = new LinkedList<Farm>();
 
         FarmFactory farmFactory = new FarmFactory();
         Farm newFarm = farmFactory.buildFarm("Animal");
@@ -12,7 +14,19 @@ public class HelloWorld {
         newFarm.setUnit(5);
         farmList.add(newFarm);
 
+        Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
+        String input = "Go";
 
+        Ticker ticker = new Ticker(farmList);
+
+        while(!input.equalsIgnoreCase("Y")){
+            ticker.doDay();
+
+            ticker.doNight();
+
+            System.out.println("Do want to quit (Y/N)");
+            input = scanner.next();
+        }
 
     }
 }
